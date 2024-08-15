2e3a927cf4cf2eba98b653a34a7f63f5f6f87750
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-08-15 12:48:46 +0530

    Add files via upload

---
+++ pr6/WebForm1.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="pr6.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+             Odd number :<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br />
+            Even number: <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox><br />
+            <asp:Button ID="Button1" runat="server" Text="ok" OnClick="Button1_Click" />
+        </div>
+        <div>&nbsp;</div>
+    </form>
+</body>
+</html>
---
+++ pr6/WebForm1.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace pr6
+
+{
+  //  int  o = 0, e = 0, odd, even;
+
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //int i, o = 0, e = 0, odd, even;
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int i, o = 0, odd, even;
+            int E = 0;
+
+            for ( i = 0, odd = 1, even = 2; i < 100; i++, odd = odd +2,even = even +2)
+            {
+                o = o + odd;
+                E = E + even;
+            }
+            TextBox1.Text = Convert.ToString(o);
+            TextBox2.Text = Convert.ToString(E);
+        }
+    }
+}
